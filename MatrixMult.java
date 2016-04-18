fcbca4f03a128b640716561565e6de9683b5f1d0
Richard Jung <richard.y.jung@gmail.com>
2016-04-17 23:04:08 -0700

    added strassen mult

--- MatrixMult.java
+++ MatrixMult.java
@@ -29,7 +29,8 @@ public class MatrixMult {
         }
         
 //        arrayC = traditionalMult(N, arrayA, arrayB, arrayC);
-        arrayC = divAndConqMult(N, arrayA, arrayB);
+//        arrayC = divAndConqMult(N, arrayA, arrayB);
+          arrayC = strassenMult(N, arrayA, arrayB);
         
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
@@ -66,10 +67,14 @@ public class MatrixMult {
         int[][] C = new int[N][N];  // resulting matrix
         int n = N/2;  // size of partition matrix
         
-        if(N == 1){ // base case
-            C[0][0] = A[0][0] * B[0][0];
+        if(N == 2) {
+            traditionalMult(N, A, B, C);
             return C;
         }
+//        if(N == 1){ // base case
+//            C[0][0] = A[0][0] * B[0][0];
+//            return C;
+//        }
             
         // partition the matrix into submatrices
         int [][] a11 = new int[n][n];   // top left
@@ -126,20 +131,107 @@ public class MatrixMult {
         return C;   // return result
     }
     
-//    public static int[][] strassenMult(int N, int[][] A, int [][] B) {
-//        if(N == 2) {
-//            
-//        }
-//    }
+    public static int[][] strassenMult(int N, int[][] A, int [][] B) {
+        int [][] C = new int [N][N];
+        
+        int n = N/2;  // size of partition matrix
+        
+        if(N == 2) {
+            traditionalMult(N, A, B, C);
+            return C;
+        }
+        
+        // partition the matrix into submatrices
+        int [][] a11 = new int[n][n];   // top left
+        int [][] a12 = new int[n][n];   // top right
+        int [][] a21 = new int[n][n];   // bottom left
+        int [][] a22 = new int[n][n];   // bottom right
+              
+        int [][] b11 = new int[n][n];   // top left
+        int [][] b12 = new int[n][n];   // top right
+        int [][] b21 = new int[n][n];   // bottom left
+        int [][] b22 = new int[n][n];   // bottom right
+        
+        int [][] c11 = new int[n][n];
+        int [][] c12 = new int[n][n];
+        int [][] c21 = new int[n][n];
+        int [][] c22 = new int[n][n];
+        
+        // get the 7 strassen sub-matrices
+        int [][] P = new int[n][n];
+        int [][] Q = new int[n][n];
+        int [][] R = new int[n][n];
+        int [][] S = new int[n][n];
+        int [][] T = new int[n][n];
+        int [][] U = new int[n][n];
+        int [][] V = new int[n][n];
+        
+        // copy to partition arrays
+        for(int i = 0; i < n; i++) {  // row 
+            for(int j = 0; j < n; j++) {  // column
+                a11[i][j] = A[i][j];    // top left a
+                b11[i][j] = B[i][j];    // top left b
+                    
+                a12[i][j] = A[i][j + n];    // top right a
+                b12[i][j] = B[i][j + n];    // top right b
+                
+                a21[i][j] = A[i + n][j];    // bottom left a
+                b21[i][j] = B[i + n][j];    // bottom left b
+                
+                a22[i][j] = A[i + n][j + n];    // bottom right a
+                b22[i][j] = B[i + n][j + n];    // bottom right b
+            }
+        }
+        
+        P = strassenMult(n, addMatrices(n, a11, a22), addMatrices(n, b11, b22));
+        Q = strassenMult(n, addMatrices(n, a21, a22), b11);
+        R = strassenMult(n, a11, subtractMatrices(n, b12, b22));
+        S = strassenMult(n, a22, subtractMatrices(n, b21, b11));
+        T = strassenMult(n, addMatrices(n, a11, a12), b22);
+        U = strassenMult(n, subtractMatrices(n, a21, a11), addMatrices(n, b11, b12));
+        V = strassenMult(n, subtractMatrices(n, a12, a22), addMatrices(n, b21, b22));
+        
+        c11 = subtractMatrices(n, addMatrices(n, P, S), addMatrices(n, T, V));
+        c12 = addMatrices(n, R, T);
+        c21 = addMatrices(n, Q, S);
+        c22 = subtractMatrices(n, addMatrices(n, P, R), addMatrices(n, Q, U));
+        
+        // fill up the resulting array
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                C[i][j] = c11[i][j];
+        for(int i = n; i < 2*n; i++) 
+            for(int j = 0; j < n; j++)
+                C[i][j] = c21[i-n][j];
+        for(int i = 0; i < n; i++)
+            for(int j= n; j < 2*n; j++)
+                C[i][j] = c12[i][j-n];
+        for(int i = n; i < 2*n; i++)
+            for(int j = n; j < 2*n; j++)
+                C[i][j] = c22[i-n][j-n];
+            
+        return C;   // return result
+    }
     
     public static int[][] addMatrices(int N, int[][] A, int[][] B) {
         int[][] C = new int[N][N];  // resulting matrix
         
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
-                C[i][j] = A[i][j] + B[i][j];
+                C[i][j] = A[i][j] + B[i][j];    // add matrices
             }
         }
-        return C;
+        return C;   // return resulting matrix
+    }
+    
+    private static int[][] subtractMatrices(int N, int[][] A, int [][] B) {
+        int [][] C = new int [N][N];    // resulting matrix
+        
+        for(int i = 0; i < N; i++) {    // subtract matrices
+            for(int j = 0; j < N; j++) {
+                C[i][j] = A[i][j] - B[i][j];    
+            }
+        }
+        return C;   // return resulting matrix
     }
 }
